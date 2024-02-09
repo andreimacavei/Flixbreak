31471d2c3709255da05807c5f20a6284d2220092
andreimacavei <andrei.macavei89@gmail.com>
2024-02-09 10:56:05 +0200

    Update vercel

--- api/index.js
+++ api/index.js
@@ -1,16 +1,22 @@
-const app = require('express')();
-const { v4 } = require('uuid');
+const path = require('path');
+const express = require('express');
+require('dotenv').config();
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
+const app = express();
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
-});
+// app.get('/api', (req, res) => {
+//   const path = `/api/item/test`;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+//   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+// });
+
+// app.get('/api/item/:slug', (req, res) => {
+//   const { slug } = req.params;
+//   res.end(`Item: ${slug}`);
+// });
+
+const tmdbRoutes = require('../routes/tmdbAPI');
+app.use('/api/tmdb', tmdbRoutes);
 
 module.exports = app;
